07d111a18e8caa25643e6c7ebcef1384699133b9
jdanfer <jdanfer@gmail.com>
2022-07-16 19:40:51 -0300

    Update web.php

--- intra-sapp/routes/web.php
+++ intra-sapp/routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Auth;
 //Route::get('/personas/{cargo}/eliminar',              'AdminController@deletePersona');
 
 Route::get('/', function () {
-	return view('welcome');
+	return view('home');
 });
 Auth::routes();
 
